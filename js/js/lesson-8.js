22cb1fd929396c8861fcd73888511717b531a865
Odlopez <Odlopez@indox.ru>
2018-03-18 04:55:11 +0400

    Задачки 7 и 8

---
+++ js/js/lesson-8.js
@@ -0,0 +1,25 @@
+/* Напишите цикл, который предлагает prompt ввести число, большее 100.
+Если посетитель ввёл другое число – попросить ввести ещё раз, и так далее.
+Цикл должен спрашивать число пока либо посетитель не введёт число, большее 100, либо не нажмёт кнопку Cancel (ESC). */
+
+function repeatCycleWhileNot() {
+
+  //очищаем текстовое окно
+  document.querySelector(".window-code").innerHTML = "";
+
+  // выполняем задание
+  do {
+    var userOption = prompt("Введите число большее 100", "");
+    if (isNaN(+userOption)) {
+      alert ("Вы ввели не число");
+    }
+  } while(userOption < 100 && userOption != null || isNaN(+userOption));
+
+  // записываем результат в окно
+  document.querySelector(".window-code").innerHTML = 'Задание №8' + '</br></br>' +  'do {' + '</br>' +
+  '  var userOption = prompt("Введите число большее 100", "");' + '</br>' + '  if (isNaN(+userOption)) {'
+  + '</br>' + '    alert ("Вы ввели не число");' + '</br>' + '  }' + '</br>' +
+  '} while(userOption < 100 && userOption != null || isNaN(+userOption));';
+}
+
+
