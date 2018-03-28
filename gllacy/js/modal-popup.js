6df911c420355b48b529944840ef4626e6597e4f
Odlopez <Odlopez@indox.ru>
2018-03-28 12:30:06 +0300

    исправил кривое поведение карты в IE

--- gllacy/js/modal-popup.js
+++ gllacy/js/modal-popup.js
@@ -5,7 +5,7 @@ var feedBackForm = overlay.querySelector(".feedback-form");
 var login = document.querySelector(".feedback-form__login");
 var email = document.querySelector(".feedback-form__email");
 var message = document.querySelector(".feedback-form__text");
-
+// Открываем overlay, модальное окно выезжает с помощью анимации, после чего класс с анимацией удаляется.
 modalButtonOpen.addEventListener("click", function (evt) {
   evt.preventDefault();
   overlay.classList.add("js-modal-show");
@@ -15,7 +15,7 @@ modalButtonOpen.addEventListener("click", function (evt) {
   }, 400);
   login.focus();
 });
-
+// Закрываем overlay после анимации закрытия модального окна.
 modalButtonClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedBackForm.classList.add("js-rotate-reverse");
@@ -24,21 +24,18 @@ modalButtonClose.addEventListener("click", function (evt) {
     feedBackForm.classList.remove("js-rotate-reverse");
   }, 390);
 });
-
+// Запускаем анимацию "дрожжания окна", если поля формы не заполнены.
 feedBackForm.addEventListener("submit", function (evt) {
   var charCode = evt.charCode;
-
   if (!login.value || !email.value || !message.value || login.value.match(/\s/g)) {
     evt.preventDefault();
-
     feedBackForm.classList.add("js-modal-animation");
-
     setTimeout( function() {
       feedBackForm.classList.remove("js-modal-animation");
     }, 400);
   }
 });
-
+// В случае, если модальное окно открыто, можем закрыть его с помощью клавиши "esc".
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (overlay.classList.contains("js-modal-show")) {
