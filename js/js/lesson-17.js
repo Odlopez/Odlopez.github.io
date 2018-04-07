d63a4702f6564da88c468e7593314b476dc6f4ed
Odlopez <Odlopez@inbox.ru>
2018-04-08 01:02:56 +0300

    Задание №17

---
+++ js/js/lesson-17.js
@@ -0,0 +1,24 @@
+/* Напишите функцию, которая принимает аргумент n и возвращает разницу между квадратом суммы и суммой квадратов первых n натуральных чисел. */
+'use strict'
+
+function perform17(num) {
+
+  //очищаем текстовое окно
+  document.querySelector(".window-code").innerHTML = "";
+
+  // выполняем задание
+  alert ( sumSquareDifference(num) );
+
+  // записываем результат в окно
+  document.querySelector(".window-code").innerHTML = "Задание №16" + "</br></br>" + "const sumSquareDifference = " + sumSquareDifference;
+}
+
+const sumSquareDifference = (num) => {
+  let sumOfSquare = 0;
+  let squareOfSum = 0;
+  for (let i = 0; i <= num; i++) {
+    sumOfSquare += Math.pow(i, 2);
+    squareOfSum += i;
+  }
+  return Math.pow(squareOfSum, 2) - sumOfSquare;
+}
