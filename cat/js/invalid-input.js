399e11bc96c692a109b16c8b60b7b86c37ea4abd
Odlopez <Odlopez@inbox.ru>
2018-05-14 17:33:19 +0300

    завершает проект

--- cat/js/invalid-input.js
+++ cat/js/invalid-input.js
@@ -1,17 +1,1 @@
-const requiredIputs = document.querySelectorAll('input[required]');
-
-for (let i = 0; i < requiredIputs.length; i++) {
-  // можно использовать внутри цикла let вместо var и тогда не придется наворачивать лишнюю функцию
-  // борясь с замыканиями. Вот только IE без лишней функции все равно не работает. Оставил ее.
-  const x = function (i) {
-    requiredIputs[i].addEventListener('input', function() {
-
-    if (requiredIputs[i].value !== '') {
-      requiredIputs[i].classList.add('input-text--required');
-    } else {
-      requiredIputs[i].classList.remove('input-text--required');
-    }
-    })
-  };
-  x(i);
-}
+for(var requiredIputs=document.querySelectorAll("input[required]"),i=0;i<requiredIputs.length;i++){var x=function(e){requiredIputs[e].addEventListener("input",function(){""!==requiredIputs[e].value?requiredIputs[e].classList.add("input-text--required"):requiredIputs[e].classList.remove("input-text--required")})};x(i)}
