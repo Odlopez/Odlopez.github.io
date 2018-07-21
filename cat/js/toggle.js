de47aa1802ff96414628f0ed8d1bcc0e5e25ff3d
Odlopez <odlopez@inbox.ru>
2018-07-21 12:52:55 +0300

    Убирает толстого кота за слайдер

--- cat/js/toggle.js
+++ cat/js/toggle.js
@@ -7,6 +7,11 @@
       TABLET: 'left: 8px;',
       DESKTOP: 'left: -18px;'
     },
+    IMAGE_BEFORE: {
+      MOBILE: '',
+      TABLET: 'left: 51%;',
+      DESKTOP: 'left: 0px;'
+    },
     IMAGE_WRAP: {
       MOBILE: '',
       TABLET: 'left: 50%;',
@@ -18,6 +23,10 @@
       DESKTOP: 'left: 50%;'
     },
   };
+  var IMAGE_BEFORE_COORDINATE = {
+    TABLET: '51%',
+    DESKTOP: '0px'
+  };
   var SCREEN = {
     TABLET: 768,
     DESKTOP: 1300
@@ -26,7 +35,9 @@
   var range = document.querySelector('.toggle__range');
   var slider = document.querySelector('.toggle__slider');
   var imageWrap = document.querySelector('.example__image-wrap');
-  var image = document.querySelector('.example__image--after');
+  var imageAfter = document.querySelector('.example__image--after');
+  var imageBefore = document.querySelector('.example__image--before');
+  var checkPoint = range.offsetWidth / 4;
 
   // Функция, позволяющая задавать стили по любому селектору. Работает только в хроме.
   var addRules = (function (style) {
@@ -45,15 +56,18 @@
   var onWindowResize = function () {
     if (window.innerWidth < SCREEN.TABLET) {
       imageWrap.style.cssText = STYLES.IMAGE_WRAP.MOBILE;
-      image.style.cssText = STYLES.IMAGE.MOBILE;
+      imageAfter.style.cssText = STYLES.IMAGE.MOBILE;
+      imageBefore.style.cssText = STYLES.IMAGE_BEFORE.MOBILE;
       slider.style.cssText = STYLES.SLIDER.MOBILE;
     } else if (window.innerWidth < SCREEN.DESKTOP) {
       imageWrap.style.cssText = STYLES.IMAGE_WRAP.TABLET;
-      image.style.cssText = STYLES.IMAGE.TABLET;
+      imageAfter.style.cssText = STYLES.IMAGE.TABLET;
+      imageBefore.style.cssText = STYLES.IMAGE_BEFORE.TABLET;
       slider.style.cssText = STYLES.SLIDER.TABLET;
     } else {
       imageWrap.style.cssText = STYLES.IMAGE_WRAP.DESKTOP;
-      image.style.cssText = STYLES.IMAGE.DESKTOP;
+      imageAfter.style.cssText = STYLES.IMAGE.DESKTOP;
+      imageBefore.style.cssText = STYLES.IMAGE_BEFORE.DESKTOP;
       slider.style.cssText = STYLES.SLIDER.DESKTOP;
     }
   };
@@ -62,7 +76,8 @@
   var onSliderMousedown = function (e) {
     var shift = e.clientX - e.target.offsetLeft;
     var imageWrapShift = e.clientX - imageWrap.offsetLeft;
-    var imageCoordinateX = image.offsetLeft;
+    var imageCoordinateX = imageAfter.offsetLeft;
+    var imageBeforeCoordinateX = (window.innerWidth > SCREEN.TABLET && window.innerWidth < SCREEN.DESKTOP) ? IMAGE_BEFORE_COORDINATE.TABLET : IMAGE_BEFORE_COORDINATE.DESKTOP;
     var startX = e.clientX;
 
     // Обработчик события движения мыши
@@ -72,7 +87,12 @@
       if (evtMove.clientX - shift > 0 && evtMove.clientX - shift < range.offsetWidth) {
         slider.style.left = evtMove.clientX - shift + 'px';
         imageWrap.style.left = evtMove.clientX - imageWrapShift + 'px';
-        image.style.left = imageCoordinateX - (evtMove.clientX - startX) + 'px';
+        imageAfter.style.left = imageCoordinateX - (evtMove.clientX - startX) + 'px';
+
+
+        if (slider.offsetLeft < checkPoint) {
+          imageBefore.style.cssText = 'left: calc(' + imageBeforeCoordinateX + ' + ' + (checkPoint - slider.offsetLeft) + 'px);';
+        }
       }
 
     };
