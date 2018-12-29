76cbfcd3164f36bc297810327f7d715396d5e4ed
Odlopez <odlopez@inbox.ru>
2018-12-29 09:52:20 +0300

    Добавляет пакману еду

--- js/pacman.js
+++ js/pacman.js
@@ -15,10 +15,12 @@
     right: [20, 30, 40, 50, 60, 70, 80],
     width: [50, 60, 70, 80]
   };
-  var MARGIN = 5;
+  var MARGIN = 1;
+  var MINI_TRIANGLE_COEFFICIENT = 1.5;
   var PACMAN_COLOR = 'gold';
+  var POINT_COLOR = '#dce2aa';
   var BACKGROUND_COLOR = '#25212d';
-  var REPEAT_DURATION = [5000, 15000];
+  var REPEAT_DURATION = [5000, 17000];
   var canvas = document.createElement('canvas');
   var ctx = canvas.getContext('2d');
 
@@ -26,6 +28,60 @@
     from = from || 0;
 
     return Math.round(Math.random() * (to - from) + from);
+  };
+
+  /**
+   * Возвращает массив координат для точек - еды пакмана
+   * @param {Number} quantity
+   * @param {Number} startPoint
+   * @param {Number} coordY
+   * @param {Number} pointRadius
+   */
+  var getPointsCoord = function (quantity, startPoint, coordY, pointRadius) {
+    var coordArray = [];
+
+    for (let i = 0; i < quantity; i++) {
+      coordArray[i] = [startPoint, coordY];
+
+      startPoint += pointRadius * 5;
+    }
+
+    return coordArray;
+  };
+
+  /**
+   * Отрисовывает круг на канвасе
+   * @param {CanvasContext} ctx
+   * @param {Number} start
+   * @param {Number} radius
+   * @param {String} fill
+   */
+  var createCircle = function (ctx, startX, startY, radius, fill) {
+    ctx.beginPath();
+    ctx.fillStyle = fill;
+    ctx.arc(startX, startY, radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  };
+
+  /**
+   *
+   * @param {CanvasContext} ctx
+   * @param {Number} startX
+   * @param {Number} startY
+   * @param {Number} height
+   * @param {Number} intensity
+   * @param {String} fill
+   * @param {Number} progress
+   */
+  var createTriangle = function (ctx, startX, startY, height, intensity, fill, progress) {
+    ctx.beginPath();
+    ctx.fillStyle = fill;
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(startX + height + MARGIN, startY - (height * Math.sin(Math.PI * intensity * progress)));
+    ctx.lineTo(startX + height + MARGIN, startY + (height * Math.sin(Math.PI * intensity * progress)));
+    ctx.fill();
+    ctx.closePath();
   };
 
   /**
@@ -50,7 +106,12 @@
    */
   var getRenderPacmanFunc = function (ctx) {
     var radius = PACMAN_OPTIONS.radius[getRandomNumber(PACMAN_OPTIONS.radius.length - 1)];
+    var miniTriangleRadius = radius / MINI_TRIANGLE_COEFFICIENT;
     var eatingIntensity = PACMAN_OPTIONS.eatingIntensity[getRandomNumber(PACMAN_OPTIONS.eatingIntensity.length - 1)];
+    var pointRadius = radius * 0.2;
+    var startPoint = getCoordX(radius, 0);
+    var pointQuantity = ScreenSize.WIDTH / pointRadius / 5;
+    var pointsCoordArray = getPointsCoord(pointQuantity, startPoint, radius, pointRadius);
 
     return function (progress) {
       // очищаем полотно
@@ -59,23 +120,27 @@
       var startX = getCoordX(radius, progress);
 
       // отрисовываем круг
-      ctx.beginPath();
-      ctx.fillStyle = PACMAN_COLOR;
-      ctx.arc(startX, radius, radius, 0, Math.PI * 2);
-      ctx.fill();
-      ctx.closePath();
+      createCircle(ctx, startX, radius, radius, PACMAN_COLOR);
+
+      // отрисовываем треугольник
+      createTriangle(ctx, startX, radius, radius, eatingIntensity, BACKGROUND_COLOR, progress);
+
+      // отрисовываем точки
+      for (let i = 0; i < pointsCoordArray.length; i++) {
+        if (pointsCoordArray[i][0] < startX + pointRadius * 3) {
+          continue;
+        }
+        createCircle(ctx, pointsCoordArray[i][0], pointsCoordArray[i][1], pointRadius, POINT_COLOR);
+      }
 
       // открисовываем треугольник
-      ctx.beginPath();
-      ctx.fillStyle = BACKGROUND_COLOR;
-      ctx.moveTo(startX, radius);
-      ctx.lineTo(startX + radius + MARGIN, radius - (radius * Math.sin(Math.PI * eatingIntensity * progress)));
-      ctx.lineTo(startX + radius + MARGIN, radius + (radius * Math.sin(Math.PI * eatingIntensity * progress)));
-      ctx.fill();
-      ctx.closePath();
+      createTriangle(ctx, startX, radius, miniTriangleRadius, eatingIntensity, BACKGROUND_COLOR, progress);
     }
   };
 
+  /**
+   * Заканчивает анимацию, очищает полотно и запускает через рандомное время еще одну анимацию
+   */
   var endAnimate = function () {
     clearCanvas(ctx);
 
